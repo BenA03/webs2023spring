239f4032321f94a96986c80796e5c9ad5abb23ab
BenA03 <bma54@pitt.edu>
2023-03-23 10:32:56 -0700

    small tweak to contact response message

--- docs/js/form.js
+++ docs/js/form.js
@@ -2,5 +2,5 @@ var form = document.querySelector('.pageclip-form')
 Pageclip.form(form, {
   onSubmit: function (event) { },
   onResponse: function (error, response) { },
-  successTemplate: '<span>Submitted! We will respond as soon as possible. Thank you!</span>'
+  successTemplate: '<span>Submitted! We will get back to you as soon as possible. Thank you!</span>'
 })
