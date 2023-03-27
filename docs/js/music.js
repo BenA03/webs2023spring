871f13fa5003328b2d78b687e3bd93b00fc8d20c
BenA03 <bma54@pitt.edu>
2023-03-27 11:19:55 -0700

    buttons now toggle show/hides for cards

--- docs/js/music.js
+++ docs/js/music.js
@@ -11,10 +11,21 @@ function init() {
 function select(e)
 {
     const yearList = document.getElementsByClassName("page-item");
+    const displays = document.getElementsByClassName("collapse");
+
+    for (let i = 0, len = displays.length; i < len; i++) {
+        displays[i].style.display = 'none';
+    }
+
+    let thingToShow;
+    let target;
     for (let i = 0, len = yearList.length; i < len; i++) {
         if (yearList[i].id == this.id)
         {
             yearList[i].classList.add('year-selected');
+            target = this.id.substring(1);
+            thingToShow = document.getElementById(target);
+            thingToShow.style.display = 'block';
         }
         else
         {
