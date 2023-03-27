4a89d1608b3491e120042059112a3756f7655da4
BenA03 <bma54@pitt.edu>
2023-03-27 15:05:52 -0700

    fixed bug where button would not react to input

--- docs/js/music.js
+++ docs/js/music.js
@@ -6,12 +6,14 @@ function init() {
     for (let i = 0, len = yearList.length; i < len; i++) {
         yearList[i].addEventListener('click', select, false);
     }
+    const show = document.getElementById("2022");
+    show.style.display = 'block';
 }
 
 function select(e)
 {
     const yearList = document.getElementsByClassName("page-item");
-    const displays = document.getElementsByClassName("collapse");
+    const displays = document.getElementsByClassName("collapse-1");
 
     for (let i = 0, len = displays.length; i < len; i++) {
         displays[i].style.display = 'none';
