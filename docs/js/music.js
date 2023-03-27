ef8f798fbde187377fc33bf403be1612806f5009
BenA03 <bma54@pitt.edu>
2023-03-27 09:16:37 -0700

    set up buttons for music page

--- docs/js/music.js
+++ docs/js/music.js
@@ -4,12 +4,21 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
     const yearList = document.getElementsByClassName("page-item");
     for (let i = 0, len = yearList.length; i < len; i++) {
-        yearList[i].addEventListener('click', select(), false);
+        yearList[i].addEventListener('click', select, false);
     }
 }
 
-function select()
+function select(e)
 {
-    console.log("I have been clicked");
-    console.log(this.id);
+    const yearList = document.getElementsByClassName("page-item");
+    for (let i = 0, len = yearList.length; i < len; i++) {
+        if (yearList[i].id == this.id)
+        {
+            yearList[i].classList.add('year-selected');
+        }
+        else
+        {
+            yearList[i].classList.remove('year-selected');
+        }
+    }
 }
